bbd0530bb683971ccdee17ab37076fbe8c27caed
shablenko@sednor.net <shablenko@sednor.net>
2019-05-20 17:47:10 +0300

    set up project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './styles/main.scss';
+// import { Route, Redirect, Switch } from 'react-router';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          Hello React
+          {/*<Switch>*/}
+          {/*  */}
+          {/*</Switch>*/}
+        </div>
+    );
+  }
+}
+
+export default App;
